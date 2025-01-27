0867f9f69c70cd589a96b254fc7d84239381f7e5
profesor-julioeld <profesor.julioeld@iesazarquiel.es>
2025-01-27 23:41:43 +0100

    Añado Tema6 con los puntos 6_1,6_2 y 6_3 Añado OrdenacionSpain para aplicar Alfabeto Español en el orden e igualdad ante acentos si procede Tablero y Ejercicio5_InicioTest pasan de CRLF a LF

--- src/main/java/tema5/ejerciciosFinales/Punto5_Ejercicio7_unPocoGraficos/Ejercicio5_inicioTest.java
+++ src/main/java/tema5/ejerciciosFinales/Punto5_Ejercicio7_unPocoGraficos/Ejercicio5_inicioTest.java
@@ -5,25 +5,25 @@ package tema5.ejerciciosFinales.Punto5_Ejercicio7_unPocoGraficos;
 public class Ejercicio5_inicioTest {
 
 	public static void main(String[] args) {
-		Tablero t=new Tablero(3);
+		Tablero t=new Tablero(5);
 		t.setTitulo(" Intento de juego de las tres en Raya");
 		// Espera 10 segundos a que pulse
 		while (t.nextCasilla(10)) { // Si no hay donde pulsar se acaba
-			boolean hanPulsado=t.getPulsado();
-			t.setCartelUsuario("Ha pulsado alguien? "+(hanPulsado?"Sí":"No"));
+			boolean hanPulsado = t.getPulsado();
+			t.setCartelUsuario("Ha pulsado alguien? " + (hanPulsado ? "Sí" : "No"));
 			if (hanPulsado) {
-				int fila=t.getFila();
-				int columna=t.getColumna();
-				char letra=t.getLetra();
+				int fila = t.getFila();
+				int columna = t.getColumna();
+				char letra = t.getLetra();
 				t.setCartelUsuario(" Ponga la letra H");
-				t.writeCasilla(fila,columna,'H');
+				t.writeCasilla(fila, columna, 'H');
 			}
 		}
 		t.setCartelUsuario("espero 5 segundos y cierro");
 		try {Thread.sleep(5000);} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		t.close();
+		//t.close();
 	}
 
 }
